567fecb3ac410298886547b07d8adbcb33ec37f6
Sparrow-Tech06 <cpal980075@gmail.com>
2026-05-21 11:34:17 +0530

    Delete js/earn.js

--- js/earn.js
+++
@@ -1,8 +0,0 @@
-let coins = localStorage.getItem("coins") || 0;
-
-function earnCoins(){
-  coins = parseInt(coins) + 50;
-  localStorage.setItem("coins", coins);
-
-  document.getElementById("msg").innerText = "You earned 50 coins 💰";
-}
